74155eca7589af53cda01929aff6fa060755f229
Natalia Braz <naabraz@gmail.com>
2018-10-17 12:49:24 -0300

    Cleaning form when adding comment

--- frontend/src/components/AddComment.js
+++ frontend/src/components/AddComment.js
@@ -8,6 +8,26 @@ import { handleAddPostComment } from '../actions/comments'
 
 class AddComment extends Component {
 
+  state = {
+    author: '',
+    body: '',
+  }
+
+  handleChangeAuthor = (e) => {
+    const author = e.target.value
+    this.setState(() => ({
+      author,
+    }))
+  }
+
+  handleChangeBody = (e) => {
+    const body = e.target.value
+
+    this.setState(() => ({
+      body,
+    }))
+  }
+
   handleSubmit = (e) => {
     e.preventDefault()
 
@@ -22,13 +42,20 @@ class AddComment extends Component {
     }
 
     this.props.dispatch(handleAddPostComment(commentPayload))
+
+    this.setState(() => ({
+      author: '',
+      body: ''
+    }))
   }
 
   render() {
+    const { author, body } = this.state
+
     return(
       <form onSubmit={this.handleSubmit}>
-        <input type="text" name="author" placeholder="Name" required />
-        <textarea name="body" placeholder="Comment" required />
+        <input type="text" name="author" placeholder="Name" required value={author} onChange={this.handleChangeAuthor} />
+        <textarea name="body" placeholder="Comment" required value={body} onChange={this.handleChangeBody} />
         <button>Add Comment</button>
       </form>
     )
