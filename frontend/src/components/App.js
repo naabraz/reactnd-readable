b17fba004f5a095333a8d09ffdc164477fbc88ba
Natalia Braz <naabraz@gmail.com>
2018-10-21 17:44:04 -0300

    Changing App to stateless functional component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import LoadingBar from 'react-redux-loading'
 
@@ -9,29 +9,26 @@ import EditPost from './post/EditPost'
 import EditComment from './comment/EditComment'
 import Nav from './shared/Nav'
 
-class App extends Component {
-
-  render() {
-    return (
-      <Router>
-        <Fragment>
-          <LoadingBar />
-          <div className='container'>
-            <Nav />
-            <h1>Readable</h1>
-              <Switch>
-                <Route path='/' exact component={PostsList} />
-                <Route path='/new' exact component={AddPost} />
-                <Route path='/:category' exact component={PostsList} />
-                <Route path='/:category/:id' exact component={PostDetails} />
-                <Route path='/post/edit/:id' exact component={EditPost} />
-                <Route path='/comment/edit/:id' exact component={EditComment} />
-              </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    )
-  }
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <LoadingBar />
+        <div className='container'>
+          <Nav />
+          <h1>Readable</h1>
+            <Switch>
+              <Route path='/' exact component={PostsList} />
+              <Route path='/new' exact component={AddPost} />
+              <Route path='/:category' exact component={PostsList} />
+              <Route path='/:category/:id' exact component={PostDetails} />
+              <Route path='/post/edit/:id' exact component={EditPost} />
+              <Route path='/comment/edit/:id' exact component={EditComment} />
+            </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  )
 }
 
 export default App
